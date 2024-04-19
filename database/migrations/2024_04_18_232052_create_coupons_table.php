35707e0e59c661325c98d8c25afaca4e6fca1962
MedELHattab <elhattabmohammedelarbi.com>
2024-04-19 08:59:29 +0100

    I added the coupon and the order migration

--- database/migrations/2024_04_18_232052_create_coupons_table.php
+++ database/migrations/2024_04_18_232052_create_coupons_table.php
@@ -4,24 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCouponsTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('coupons', function (Blueprint $table) {
             $table->id();
+            $table->string('code')->unique();
+            $table->decimal('percentage_discount', 5, 2); // Percentage discount value
+            $table->integer('usage_limit')->nullable(); // Number of times coupon can be used (null means unlimited)
+            $table->dateTime('expires_at')->nullable(); // Expiry date of the coupon
             $table->timestamps();
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('coupons');
     }
-};
+}
