2e204c5398858c4539f6afaf450102390c84da04
MedELHattab <elhattabmohammedelarbi.com>
2024-04-22 09:19:50 +0100

    I created the Gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('isAdmin',function($user){
+            return $user->role =='admin';
+        });
+
+        Gate::define('isUser',function($user){
+            return $user->role =='user';
+        });
     }
 }
